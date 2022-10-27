36f379149870e4679eb605709726f42f4ff723d0
jiaulislam <kmjiaulislam@gmail.com>
2022-10-27 21:36:38 +0600

    staged webpack

--- app/index.php
+++ app/index.php
@@ -1,1 +1,17 @@
-<?php phpinfo(); ?>
+<!doctype html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Bootstrap w/ Webpack</title>
+  </head>
+  <body>
+    <?php include_once 'assets/templates/layout/header.php'?>
+    <div class="container py-4 px-3 mx-auto">
+      <h1>Hello, Bootstrap and Webpack!</h1>
+      <button class="btn btn-primary">Primary button</button>
+    </div>
+    <?php include_once 'assets/templates/layout/footer.php'?>
+    <script src="./assets/dist/main.js"></script>
+  </body>
+</html>
